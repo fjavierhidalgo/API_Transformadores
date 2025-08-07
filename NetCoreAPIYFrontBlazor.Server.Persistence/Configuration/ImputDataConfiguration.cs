5fadfbb932ac13958721b84f5e58c34c6644e1ea
Javier Hidalgo Rojo <javierhidalgo@soliss.com>
2025-08-07 08:14:29 +0200

    Cambios 07/08/2025

--- NetCoreAPIYFrontBlazor.Server.Persistence/Configuration/ImputDataConfiguration.cs
+++ NetCoreAPIYFrontBlazor.Server.Persistence/Configuration/ImputDataConfiguration.cs
@@ -24,12 +24,12 @@ namespace NetCoreAPIYFrontBlazor.Server.Persistence.Configuration
 
             builder.Property(n => n.Standard).HasColumnType($"varchar(20)");
             builder.Property(n => n.Rev).HasColumnType($"varchar(20)");
-            builder.Property(n => n.Type).HasColumnType($"varchar(20)");
+            builder.Property(n => n.Type).HasColumnType($"varchar(50)");
             builder.Property(n => n.OFNum).HasColumnType($"varchar(20)");
-            builder.Property(n => n.Designer).HasColumnType($"varchar(20)");
-            builder.Property(n => n.ConectionHV1).HasColumnType($"varchar(20)");
-            builder.Property(n => n.ConectionLV1).HasColumnType($"varchar(20)");
-            builder.Property(n => n.ConectionVacio2).HasColumnType($"varchar(20)");
+            builder.Property(n => n.Designer).HasColumnType($"varchar(40)");
+            builder.Property(n => n.ConectionHV1).HasColumnType($"varchar(10)");
+            builder.Property(n => n.ConectionLV1).HasColumnType($"varchar(10)");
+            builder.Property(n => n.ConectionVacio2).HasColumnType($"varchar(10)");
             builder.Property(n => n.HVBIL).HasColumnType($"varchar(20)");
             builder.Property(n => n.LVBIL).HasColumnType($"varchar(20)");
             builder.Property(n => n.HVKIND).HasColumnType($"varchar(20)");
@@ -43,36 +43,7 @@ namespace NetCoreAPIYFrontBlazor.Server.Persistence.Configuration
             builder.Property(n => n.NoiseKSB).HasColumnType($"varchar(1)");
             builder.Property(n => n.NoiseKV).HasColumnType($"varchar(1)");
 
-            /*
-                        public string? Standard { get; set; }
-
-                    public string? Rev { get; set; }
-                    public string? Type { get; set; }
-                    public string? OFNum { get; set; }
-                    public string? Designer { get; set; }
-
-                    public string? ConectionHV1 { get; set; }
-                    public string? ConectionLV1 { get; set; }
-                    public string? ConectionVacio2 { get; set; }
-
-                     public string? HVBIL { get; set; }
-        public string? LVBIL { get; set; }
-        public string? HVKIND { get; set; }
-        public string? LVKIND { get; set; }
-        public string? NLLosses { get; set; }
-        public string? Llosses { get; set; }
-        public string? HVMAT { get; set; }
-        public string? LVMAT { get; set; }
-        public string? NoiseKP { get; set; }
-        public string? NoiseKHi { get; set; }
-        public string? NoiseKSB { get; set; }
-        public string? NoiseKV { get; set; }
-
-
-
-
-
-            */
+          
         }
     }
 
---
+++ NetCoreAPIYFrontBlazor.Server.Persistence/Migrations/20250807060228_Modif_InputDataAmpliacion.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetCoreAPIYFrontBlazor.Server.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modif_InputDataAmpliacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Altitude",
+                table: "InputsData",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConectionHV1",
+                table: "InputsData",
+                type: "varchar(10)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConectionLV1",
+                table: "InputsData",
+                type: "varchar(10)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConectionVacio2",
+                table: "InputsData",
+                type: "varchar(10)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "InputsData",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Designer",
+                table: "InputsData",
+                type: "varchar(40)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Foils",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HVBIL",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HVKIND",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HVMAT",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "KRAB",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "KRBT",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LVBIL",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LVKIND",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LVMAT",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LineVoltGuion",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LineVoltHV1",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LineVoltLV1",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LineVoltVacio",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LineVoltVacio2",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Llosses",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NLLosses",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Noise",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NoiseKHi",
+                table: "InputsData",
+                type: "varchar(1)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NoiseKP",
+                table: "InputsData",
+                type: "varchar(1)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NoiseKSB",
+                table: "InputsData",
+                type: "varchar(1)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NoiseKV",
+                table: "InputsData",
+                type: "varchar(1)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OFNum",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rev",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SC",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Standard",
+                table: "InputsData",
+                type: "varchar(20)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TMax",
+                table: "InputsData",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TurnsLV1",
+                table: "InputsData",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "InputsData",
+                type: "varchar(50)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Altitude",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "ConectionHV1",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "ConectionLV1",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "ConectionVacio2",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Designer",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Foils",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "HVBIL",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "HVKIND",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "HVMAT",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "KRAB",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "KRBT",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LVBIL",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LVKIND",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LVMAT",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LineVoltGuion",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LineVoltHV1",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LineVoltLV1",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LineVoltVacio",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "LineVoltVacio2",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Llosses",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "NLLosses",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Noise",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "NoiseKHi",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "NoiseKP",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "NoiseKSB",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "NoiseKV",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "OFNum",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Rev",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "SC",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Standard",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "TMax",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "TurnsLV1",
+                table: "InputsData");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "InputsData");
+        }
+    }
+}
