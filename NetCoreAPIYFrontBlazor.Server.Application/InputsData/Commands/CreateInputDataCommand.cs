f6e302c534abe026e9229fee2726170f07a36282
Javier Hidalgo Rojo <javierhidalgo@soliss.com>
2025-12-17 11:52:24 +0100

    Cambios 17/12/2025

--- NetCoreAPIYFrontBlazor.Server.Application/InputsData/Commands/CreateInputDataCommand.cs
+++ NetCoreAPIYFrontBlazor.Server.Application/InputsData/Commands/CreateInputDataCommand.cs
@@ -20,6 +20,40 @@ public class CreateInputDataCommand : IRequest<int>
     public decimal? hVTapPosRegulacion { get; set; }
     public decimal? hVTapPosMax { get; set; }
     public string? oilKind { get; set; }
-   
+
+    public string? standard { get; set; }
+    public DateTime? date { get; set; }
+    public string? rev { get; set; }
+    public string? type { get; set; }
+    public string? oFNum { get; set; }
+    public string? designer { get; set; }
+    public int? lineVoltHV1 { get; set; }
+    public int? lineVoltGuion { get; set; }
+    public int? lineVoltLV1 { get; set; }
+    public int? lineVoltVacio { get; set; }
+    public int? lineVoltVacio2 { get; set; }
+    public string? conectionHV1 { get; set; }
+    public string? conectionLV1 { get; set; }
+    public string? conectionVacio2 { get; set; }
+    public int? turnsLV1 { get; set; }
+    public int? foils { get; set; }
+    public decimal? altitude { get; set; }
+    public decimal? tMax { get; set; }
+    public string? hVBIL { get; set; }
+    public string? lVBIL { get; set; }
+    public string? hVKIND { get; set; }
+    public string? lVKIND { get; set; }
+    public string? nLLosses { get; set; }
+    public string? llosses { get; set; }
+    public string? hVMAT { get; set; }
+    public string? lVMAT { get; set; }
+    public int? noise { get; set; }
+    public int? sC { get; set; }
+    public string? noiseKP { get; set; }
+    public string? noiseKHi { get; set; }
+    public string? noiseKSB { get; set; }
+    public string? noiseKV { get; set; }
+    public int? kRBT { get; set; }
+    public int? kRAB { get; set; }
 
 }
--- NetCoreAPIYFrontBlazor.Server.Application/InputsData/Commands/CreateInputDataCommandHandler.cs
+++ NetCoreAPIYFrontBlazor.Server.Application/InputsData/Commands/CreateInputDataCommandHandler.cs
@@ -19,17 +19,14 @@ public class CreateInputDataCommandHandler : IRequestHandler<CreateInputDataComm
 
     public async Task<int> Handle(CreateInputDataCommand request, CancellationToken cancellationToken)
     {
-
-        
         InputData? valorLectura = await _serverContext
                 .InputsData
                 .FirstOrDefaultAsync(n => n.Id == request.id);
 
-       
         if (valorLectura is null)
         {
             //Si no existe lo creamos
-            InputData valorNuevo = new ();
+            InputData valorNuevo = new();
             valorNuevo.TransformadorId = request.transformadorId ?? 0;
             valorNuevo.Project = request.project ?? "";
             valorNuevo.Customer = request.customer ?? "";
@@ -43,7 +40,43 @@ public class CreateInputDataCommandHandler : IRequestHandler<CreateInputDataComm
             valorNuevo.HVTapPosRegulacion = request.hVTapPosRegulacion;
             valorNuevo.HVTapPosMax = request.hVTapPosMax;
             valorNuevo.OilKind = request.oilKind ?? "";
-           
+
+            // Propiedades adicionales que faltaban
+            valorNuevo.Standard = request.standard ?? "";
+            valorNuevo.Date = request.date;
+            valorNuevo.Rev = request.rev ?? "";
+            valorNuevo.Type = request.type ?? "";
+            valorNuevo.OFNum = request.oFNum ?? "";
+            valorNuevo.Designer = request.designer ?? "";
+            valorNuevo.LineVoltHV1 = request.lineVoltHV1;
+            valorNuevo.LineVoltGuion = request.lineVoltGuion;
+            valorNuevo.LineVoltLV1 = request.lineVoltLV1;
+            valorNuevo.LineVoltVacio = request.lineVoltVacio;
+            valorNuevo.LineVoltVacio2 = request.lineVoltVacio2;
+            valorNuevo.ConectionHV1 = request.conectionHV1 ?? "";
+            valorNuevo.ConectionLV1 = request.conectionLV1 ?? "";
+            valorNuevo.ConectionVacio2 = request.conectionVacio2 ?? "";
+            valorNuevo.TurnsLV1 = request.turnsLV1;
+            valorNuevo.Foils = request.foils;
+            valorNuevo.Altitude = request.altitude;
+            valorNuevo.TMax = request.tMax;
+            valorNuevo.HVBIL = request.hVBIL ?? "";
+            valorNuevo.LVBIL = request.lVBIL ?? "";
+            valorNuevo.HVKIND = request.hVKIND ?? "";
+            valorNuevo.LVKIND = request.lVKIND ?? "";
+            valorNuevo.NLLosses = request.nLLosses ?? "";
+            valorNuevo.Llosses = request.llosses ?? "";
+            valorNuevo.HVMAT = request.hVMAT ?? "";
+            valorNuevo.LVMAT = request.lVMAT ?? "";
+            valorNuevo.Noise = request.noise;
+            valorNuevo.SC = request.sC;
+            valorNuevo.NoiseKP = request.noiseKP ?? "";
+            valorNuevo.NoiseKHi = request.noiseKHi ?? "";
+            valorNuevo.NoiseKSB = request.noiseKSB ?? "";
+            valorNuevo.NoiseKV = request.noiseKV ?? "";
+            valorNuevo.KRBT = request.kRBT;
+            valorNuevo.KRAB = request.kRAB;
+
             await _serverContext.InputsData.AddAsync(valorNuevo);
         }
         else
@@ -60,11 +93,46 @@ public class CreateInputDataCommandHandler : IRequestHandler<CreateInputDataComm
             valorLectura.HVTapPosRegulacion = request.hVTapPosRegulacion;
             valorLectura.HVTapPosMax = request.hVTapPosMax;
             valorLectura.OilKind = request.oilKind ?? "";
+
+            // Propiedades adicionales para actualización
+            valorLectura.Standard = request.standard ?? "";
+            valorLectura.Date = request.date;
+            valorLectura.Rev = request.rev ?? "";
+            valorLectura.Type = request.type ?? "";
+            valorLectura.OFNum = request.oFNum ?? "";
+            valorLectura.Designer = request.designer ?? "";
+            valorLectura.LineVoltHV1 = request.lineVoltHV1;
+            valorLectura.LineVoltGuion = request.lineVoltGuion;
+            valorLectura.LineVoltLV1 = request.lineVoltLV1;
+            valorLectura.LineVoltVacio = request.lineVoltVacio;
+            valorLectura.LineVoltVacio2 = request.lineVoltVacio2;
+            valorLectura.ConectionHV1 = request.conectionHV1 ?? "";
+            valorLectura.ConectionLV1 = request.conectionLV1 ?? "";
+            valorLectura.ConectionVacio2 = request.conectionVacio2 ?? "";
+            valorLectura.TurnsLV1 = request.turnsLV1;
+            valorLectura.Foils = request.foils;
+            valorLectura.Altitude = request.altitude;
+            valorLectura.TMax = request.tMax;
+            valorLectura.HVBIL = request.hVBIL ?? "";
+            valorLectura.LVBIL = request.lVBIL ?? "";
+            valorLectura.HVKIND = request.hVKIND ?? "";
+            valorLectura.LVKIND = request.lVKIND ?? "";
+            valorLectura.NLLosses = request.nLLosses ?? "";
+            valorLectura.Llosses = request.llosses ?? "";
+            valorLectura.HVMAT = request.hVMAT ?? "";
+            valorLectura.LVMAT = request.lVMAT ?? "";
+            valorLectura.Noise = request.noise;
+            valorLectura.SC = request.sC;
+            valorLectura.NoiseKP = request.noiseKP ?? "";
+            valorLectura.NoiseKHi = request.noiseKHi ?? "";
+            valorLectura.NoiseKSB = request.noiseKSB ?? "";
+            valorLectura.NoiseKV = request.noiseKV ?? "";
+            valorLectura.KRBT = request.kRBT;
+            valorLectura.KRAB = request.kRAB;
         }
 
-         var res= await _serverContext.SaveChangesAndAuditAsync(_sessionService.UsuarioId);
+        var res = await _serverContext.SaveChangesAndAuditAsync(_sessionService.UsuarioId);
         
         return res;
-
     }
 }
